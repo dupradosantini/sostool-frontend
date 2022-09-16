d256df5e260a6324126e958fc7770660e39a1474
Luis Santini <dupradosantini@opus-software.com.br>
2022-09-15 21:06:58 -0300

    Nav-Bar adpatation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
+import { ModelResponsibilityComponent } from './components/views/model-responsibility/model-responsibility.component';
 import { ModelRoleReadComponent } from './components/views/model-role/model-role-read/model-role-read.component';
 import { WorkspaceReadComponent } from './components/views/workspace/workspace-read/workspace-read.component';
 import { WorkspaceSingleComponent } from './components/views/workspace/workspace-single/workspace-single.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path:"model-roles",
     component: ModelRoleReadComponent
+  },
+  {
+    path:"model-responsibility",
+    component: ModelResponsibilityComponent
   }
 ];
 
