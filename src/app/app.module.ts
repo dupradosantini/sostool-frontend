e7586ef11b5fc390c8bb3f35ac6c15f59a00f805
Luis Santini <dupradosantini@opus-software.com.br>
2022-09-30 09:42:01 -0300

    model resp edit button and role resp remove button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
